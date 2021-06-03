4f54982e5d95d92aefbff4c3d2b1b6f8f4daabec
Athameon <Benjamin.Nopper@freenet.de>
2021-06-03 17:52:40 +0200

    Created initial react project with routing and bootstrap

---
+++ src/Components/Header.js
@@ -0,0 +1,123 @@
+import React, { useState } from "react";
+
+const Header = (props) => {
+  const [searchInput, setSearchInput] = useState("");
+  const onSearchInputChange = ({ target }) => {
+    setSearchInput(target.value);
+  };
+
+  return (
+    <header>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          <div className="navbar-brand">ToDO: Name</div>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item dropdown">
+                <div
+                  className="nav-link dropdown-toggle"
+                  id="navbarDropdown"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  First
+                </div>
+                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <li>
+                    <div className="dropdown-item" name="first">
+                      First-First
+                    </div>
+                  </li>
+                  <li>
+                    <div className="dropdown-item" name="first">
+                      First-Second
+                    </div>
+                  </li>
+                </ul>
+              </li>
+              <li className="nav-item dropdown">
+                <div
+                  className="nav-link dropdown-toggle"
+                  id="navbarDropdown"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Second
+                </div>
+                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <li>
+                    <div className="dropdown-item" name="second">
+                      Second-First
+                    </div>
+                  </li>
+                  <li>
+                    <div className="dropdown-item" name="second">
+                      Second-Second
+                    </div>
+                  </li>
+                </ul>
+              </li>
+              <li className="nav-item dropdown">
+                <div
+                  className="nav-link dropdown-toggle"
+                  id="navbarDropdown"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Third
+                </div>
+                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <li>
+                    <div className="dropdown-item" name="third">
+                      Third-First
+                    </div>
+                  </li>
+                  <li>
+                    <div className="dropdown-item" name="third">
+                      Third-Second
+                    </div>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+            <form
+              className="d-flex"
+              onSubmit={(event) => {
+                event && event.preventDefault();
+                //TODO: Do the search
+              }}
+            >
+              <input
+                onChange={(event) => onSearchInputChange(event)}
+                className="form-control me-2"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+              />
+              <button className="btn btn-outline-success" type="submit">
+                Search
+              </button>
+            </form>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+
+export default Header;
---
+++ src/Components/Main.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Main = (props) => {
+  return(
+    <main>
+      <h1>This is the Main</h1>
+    </main>
+  )
+}
+
+export default Main;
