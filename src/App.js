96acdc3751f3ede97b62b55a7bdece19c718b46c
Athameon <Benjamin.Nopper@freenet.de>
2021-06-06 13:37:31 +0200

    Implemented fetch all data from contentful api.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 import './App.css';
+
 import Footer from './Components/Footer';
 import Header from './Components/Header';
 import Main from './Components/Main';
 
+import Client from './client';
+
 function App() {
+  const [content, setContent] = useState(null);
+  
+  useEffect(() => {
+    
+    Client.getEntries()
+    .then(result => {
+      console.log(result);
+      setContent(result);
+    }, (error) => {
+      throw Error("Network Error." + error)
+    })
+    .catch(error => {
+      console.log("Error occured");
+      console.error(error);
+    })
+  }, [])
+
   return (
     <div>
       <Header />
       <Switch>
-      <Route path="/">
-          <Main />
+        <Route path="/">
+          { content && <Main content={content} />}
         </Route>
       </Switch>
       <Footer />
