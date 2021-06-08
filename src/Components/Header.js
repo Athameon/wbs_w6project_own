38014d5bc5baa803afde011618f25011e74579f2
zapatacarlos <49616574+zapatacarlos@users.noreply.github.com>
2021-06-08 12:32:36 +0200

    Style header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import blockchain_logo from "./../assets/icon_blockchain.png"
 
 const Header = (props) => {
   const [searchInput, setSearchInput] = useState("");
@@ -8,7 +9,68 @@ const Header = (props) => {
 
   return (
     <header>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+
+      <div className="prenav">
+        <div className="logoImage"></div>
+
+            <img src={blockchain_logo} alt="blockchain logo" id="blchainlogo"/>
+        
+        <div className="logoText">
+            <div className="container">
+                <div className="row">
+                    <h1>The Daily Crypto</h1>
+                </div>
+                <div className="row fs-6">
+                    <h3 className="fs-6 text-end">Powered by React & Contentful</h3>
+                </div>
+            </div>
+        </div>
+        
+        <div className="nav_ticker">
+
+            <div class="table-responsive-l">
+                <table className="table table-borderless fw-lighter table-xxl">
+                    <tbody>
+                        <tr className="fs-5 row-bottom-margin" id="currency_labels">
+                            <th>BTC</th>
+                            <th>ETH</th>
+                            <th>XRP</th>
+                            <th>BCH</th>
+                            <th>EOS</th>
+                            <th>DOGE</th>
+                        </tr>
+                    </tbody>
+
+                    <tbody>
+                        <tr className="fs-5">
+                            <th>$1.00</th>
+                            <th>$2.00</th>
+                            <th>$3.00</th>
+                            <th>$4.00</th>
+                            <th>$5.00</th>
+                            <th>$6.00</th>
+                        </tr>
+                    </tbody>
+
+                    <tbody>
+                        <tr className="fs-6">
+                            <th>+0.1%</th>
+                            <th>-0.2%</th>
+                            <th>+0.3%</th>
+                            <th>-0.4%</th>
+                            <th>+0.5%</th>
+                            <th>-0.6%</th>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+
+        </div>
+        
+        <div className="nav_links"></div>
+      </div>
+
+      <nav className="navbar navbar-expand-lg">
         <div className="container-fluid">
           <div className="navbar-brand">ToDO: Name</div>
           <button
