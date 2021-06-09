8bead3244467dfc19c469962bb4d854fbdabf4d7
Athameon <Benjamin.Nopper@freenet.de>
2021-06-09 12:29:42 +0200

    Reused the News component within the Author component.

--- src/Components/Author.js
+++ src/Components/Author.js
@@ -3,6 +3,7 @@ import Post from './Post';
 import { useParams } from 'react-router-dom';
 import marked from 'marked'
 import './Author.css'
+import News from './News';
 
 const Author = ({items}) => {
   const { name } = useParams();
@@ -10,8 +11,7 @@ const Author = ({items}) => {
     .filter(item => item.fields.name === name)[0];
   
   const picture = author.fields.profilePicture.fields.file.url;
-  const posts = items.filter(item => item.sys.contentType.sys.id === 'post')
-    .filter(item => item.fields.author && item.fields.author.fields.name === name);
+  const itemsByAuthor = items.filter(item => item.fields.author && item.fields.author.fields.name === name);
 
   return( 
     <>
@@ -20,7 +20,8 @@ const Author = ({items}) => {
       <section className='aboutText' dangerouslySetInnerHTML={{ __html: marked(author.fields.about)}} />
       <hr className='articleSeparator' />
       <h2>Articles composed by {name}:</h2>
-      {posts.map(item => <Post key={item.sys.id} {...item} />)}
+
+      <News items={itemsByAuthor} />
     </>
   )
 }
