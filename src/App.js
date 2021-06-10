77d81bd5e8ccfe581c1822adbd4560f2471e49d0
zapatacarlos <49616574+zapatacarlos@users.noreply.github.com>
2021-06-10 09:46:57 +0200

    Connect crypto ticker in header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
+// import Data from '../Mock.json';
 
 import './App.css';
 
@@ -12,46 +13,151 @@ import LoadingComponent from './Components/LoadingComponent'
 import Client from './client';
 
 function App() {
-  const [content, setContent] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
-  const [isError, setIsError] = useState(false);
-  
-  useEffect(() => {
-    setIsLoading(true);
-    setIsError(false);
-    
-    Client.getEntries()
-    .then(result => {
-      console.log(result);
-      setIsLoading(false);
-      setContent(result);
-    }, (error) => {
-      throw Error("Network Error." + error)
-    })
-    .catch(error => {
-      console.log("Error occured");
-      console.error(error);
-      setIsLoading(false);
-      setIsError(true);
-    })
-  }, [])
+    const [content, setContent] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+    const [isError, setIsError] = useState(false);
 
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route path="/error" component={Error} />
-        <Route path="/">
-          { isLoading? 
-            <LoadingComponent /> :
-            isError?
-            <Redirect to="/error" /> :
-            content && <Main content={content} />}
-        </Route>
-      </Switch>
-      <Footer />
-    </div>
-  );
+    //State for the current prices from 
+    const [values, setCurrentData] = useState(null);
+    const priceDelta = useRef(0)
+    const prevData = useRef(null);
+    // setCurrentData(Data);
+  
+    console.log("hello");
+    console.log(prevData);
+    console.log(values);
+
+    // hook for getting content from Contentful
+    useEffect(() => {
+        setIsLoading(true);
+        setIsError(false);
+        
+        Client.getEntries()
+        .then(result => {
+        console.log(result);
+        setIsLoading(false);
+        setContent(result);
+        }, (error) => {
+        throw Error("Network Error." + error)
+        })
+        .catch(error => {
+        console.log("Error occured");
+        console.error(error);
+        setIsLoading(false);
+        setIsError(true);
+        })
+    }, [])
+
+    // hook for fetching with time interval
+    useEffect(() => {
+        const interval = setInterval(() => {
+        console.log("Trigger fetch data from api.");
+        fetch("https://api.nomics.com/v1/currencies/ticker?key=e976e656db4b58f3a781f96a9918f6f3916e6849&ids=BTC,ETH,XRP,BCH,EOS,DOGE&convert=EUR&per-page=100&page=1", {
+            method: 'GET', // *GET, POST, PUT, DELETE, etc.
+            cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        })
+        .then(result => {
+            console.log(result);
+            if(result.ok) {
+            return result.json();
+            }
+            throw Error("Error");
+        }, (error => {
+            throw Error("Network Error");
+        }))
+        .then(jsonData => {
+            if (priceDelta.current !== jsonData[0].price) {
+            console.log("Update price");
+            setCurrentData(jsonData);
+            priceDelta.current = jsonData[0].price;
+            }
+            
+        })
+        .catch(error => {
+            console.error("Failed to fetch the data.");
+        })
+        }, 5000)
+        return () => {
+        clearInterval(interval);
+        }
+    }, [])
+
+    // hook for the fetch of the first render
+    useEffect(() => {
+        fetch("https://api.nomics.com/v1/currencies/ticker?key=e976e656db4b58f3a781f96a9918f6f3916e6849&ids=BTC,ETH,XRP,BCH,EOS,DOGE&convert=EUR&per-page=100&page=1", {
+        method: 'GET', // *GET, POST, PUT, DELETE, etc.
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        })
+        .then(result => {
+        console.log(result);
+        if(result.ok) {
+            return result.json();
+        }
+        throw Error("Error");
+        }, (error => {
+        throw Error("Network Error");
+        }))
+        .then(jsonData => {
+        setCurrentData(jsonData);
+        })
+        .catch(error => {
+        console.error(error);
+        })
+    }, [])
+
+    useEffect(() => {
+        prevData.current = values;
+    }, [values])
+
+ 
+    return (
+        <div>
+            {values &&
+            <Header values={values} />}
+            <Switch>
+                <Route path="/error" component={Error} />
+                <Route path="/">
+                { isLoading? 
+                    <LoadingComponent /> :
+                    isError?
+                    <Redirect to="/error" /> :
+                    content && <Main content={content} />}
+                </Route>
+            </Switch>
+
+            {values?
+            (
+            <aside>
+                <h2>Name:</h2>
+                <p>{values[0].name}</p>
+                <h2>Currency</h2>
+                <p>{values[0].currency}</p>
+                <h2>Price (€):</h2>
+                <p>{values[0].price} {prevData.current === null? "-" : values[0].price === prevData.current.price? "O" :  values[0].price > prevData.current.price? "+" : "-"}</p>
+                <p>{prevData.current && values[0].price - prevData.current.price > 0 && "+"}{prevData.current && values[0].price - prevData.current.price}</p>
+                <h2>First Trade:</h2>
+                <p>{values[0].first_trade}</p>
+                <h2>Alltime high date:</h2>
+                <p>{values[0].high_timestamp}</p>
+                <h2>Alltime high value:</h2>
+                <p>{values[0].high}</p>
+                <hr />
+                <h2>Exchange history:</h2>
+                <h3>1 Day</h3>
+                <p>{values[0]["1d"].price_change}</p>
+                <h3>7 Days</h3>
+                <p>{values[0]["7d"].price_change}</p>
+                <h3>30 Days</h3>
+                <p>{values[0]["30d"].price_change}</p>
+                <h3>356 Days</h3>
+                <p>{values[0]["365d"].price_change}</p>
+            </aside>
+            ) :
+            <p>Empty</p> 
+            }
+        <Footer />
+        </div>
+    );
 }
 
 export default App;
